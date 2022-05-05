a4cf56a3bd2969b9622d45ff48c766813369dc81
ocelot8421 <8ocelot@gmail.com>
2022-05-05 15:30:59 +0200

    EmployeeDaoTest.testThenFind is the last method

--- src/main/java/springJdbcTemp/EmployeeDao.java
+++ src/main/java/springJdbcTemp/EmployeeDao.java
@@ -1,13 +1,12 @@
 package springJdbcTemp;
 
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 public class EmployeeDao {
@@ -21,35 +20,22 @@ public class EmployeeDao {
     public long createEmployee(String name) {
         KeyHolder holder = new GeneratedKeyHolder();
 
-        jdbcTemplate.update(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
-                PreparedStatement ps = conn.prepareStatement("insert into employees(emp_name) values (?)",
-                        Statement.RETURN_GENERATED_KEYS);
-                ps.setString(1, name);
-                return ps;
-            }
+        jdbcTemplate.update(conn -> {
+            PreparedStatement ps = conn.prepareStatement("insert into employees(emp_name) values (?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, name);
+            return ps;
         }, holder);
         return holder.getKey().longValue();
     }
 
     public List<String> listEmployeeNames() {
-        return jdbcTemplate.query("select emp_name from employees order by emp_name", new RowMapper<String>() {
-            @Override
-            public String mapRow(ResultSet resultSet, int rowNum) throws SQLException {
-                return resultSet.getString("emp_name");
-            }
-        });
+        return jdbcTemplate.query("select emp_name from employees order by emp_name", (resultSet, rowNum) -> resultSet.getString("emp_name"));
     }
 
     public String findEmployeeNameById(long id) {
         return jdbcTemplate.queryForObject("select emp_name from employees where id = ?",
-                new Object[]{id}, new RowMapper<String>() {
-                    @Override
-                    public String mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        return rs.getString("emp_name");
-                    }
-                });
+                new Object[]{id}, (rs, rowNum) -> rs.getString("emp_name"));
     }
 
 }
